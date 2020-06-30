c638d9cbde096ae31e3dbf9b0b1eada813cc6257
h1ddenhyde <h1ddenhyde@yandex.ru>
2020-06-30 21:42:07 +0300

    Initial commit

---
+++ src/main/java/web/parser/carsharing/CarSharingApplication.java
@@ -0,0 +1,15 @@
+package web.parser.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CarSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/web/parser/carsharing/task/FileCreator.java
@@ -0,0 +1,47 @@
+package web.parser.carsharing.task;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileCreator {
+    String input;
+    String path;
+
+    public FileCreator(String input) {
+        this.input = input;
+    }
+
+    public boolean createFile() throws IOException, ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-z");
+        String format = dateFormat.format(new Date());
+        this.path = format + "_cars.txt";
+
+        JSONArray data = getJSON(input);
+        StringBuilder sb = new StringBuilder();
+        data.forEach(o -> sb.append("name ").append((String) ((JSONObject) o).get("name"))
+                .append(" id ").append(((JSONObject) o).get("id"))
+                .append(" latitude ").append(((JSONObject) o).get("latitude"))
+                .append(" longitude ").append(((JSONObject) o).get("longitude")).append("\n"));
+
+        Files.createFile(Paths.get(path));
+        Files.write(Paths.get(path), sb.toString().getBytes(), new StandardOpenOption[]{StandardOpenOption.APPEND});
+
+        return Files.exists(Paths.get(path));
+    }
+
+    private JSONArray getJSON(String string) throws ParseException {
+        String substring = string.substring(string.indexOf("["), string.indexOf("]") + 1);
+        JSONParser jsonParser = new JSONParser();
+        return (JSONArray) jsonParser.parse(substring);
+    }
+}
